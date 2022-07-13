737702b2d8a31768a0fc1bdd8c70804825084328
ForEars <1228456740@qq.com>
2022-07-13 12:12:46 +0800

    完成了基本配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1>黑马头条</h1>
+    <span class="iconfont icon-yanzhengma"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <button class="btn"></button>
   </div>
 </template>
 
@@ -10,4 +17,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.btn {
+  width: 347px;
+  height: 44px;
+  background-color: #6db4fb;
+  border-radius: 5px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入字体图标css样式
+import './assets/fonts/iconfont.css'
+// 引入Vant样式组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 引入移动端适配文件amfe-flexible
+import 'amfe-flexible/index.min.js'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
