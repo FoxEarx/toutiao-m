b19434c9102cd0836b0ec7f8cbff2663c1c3c155
ForEars <1228456740@qq.com>
2022-07-15 17:52:53 +0800

    完成了路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,29 @@ const routes = [
   {
     path: '/login',
     component: () => import('@/views/login')
+  },
+  {
+    path: '/',
+    component: () => import('@/views/Layout'),
+    redirect: '/home',
+    children: [
+      {
+        path: 'home',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: 'profile',
+        component: () => import('@/views/My')
+      },
+      {
+        path: 'qa',
+        component: () => import('@/views/QA')
+      },
+      {
+        path: 'video',
+        component: () => import('@/views/Video')
+      }
+    ]
   }
 ]
 
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,9 +1,57 @@
 <template>
-  <div>布局页面</div>
+  <div>
+    <router-view></router-view>
+    <van-tabbar route v-model="active">
+      <van-tabbar-item to="/home">
+        <template #icon>
+          <span class="iconfont icon-shouye"></span>
+          <span class="text">首页</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <template #icon>
+          <span class="iconfont icon-shipin"></span>
+          <span class="text">视频</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <template #icon>
+          <span class="iconfont icon-wenda"></span>
+          <span class="text">问答</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="profile">
+        <template #icon>
+          <span class="iconfont icon-wode"></span>
+          <span class="text">我的</span>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      active: ''
+    }
+  }
+}
 </script>
 
-<style></style>
+<style scoped lang="less">
+:deep(.van-tabbar-item__icon) {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 100%;
+  .text {
+    font-size: 0.32rem;
+  }
+  .iconfont {
+    font-size: 0.5333rem;
+  }
+}
+</style>
